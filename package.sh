c68ddbf8d0e03143984cc58f4481380d7de1e64a
sebasbit <sebastian.torres0426@gmail.com>
2022-07-24 18:07:23 -0500

    Actualizar package.sh

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
-unzip lib/vendor/packages/symfony1-1.4.20.zip -d lib/vendor/ \
-  && cp lib/vendor/symfony1-1.4.20/data/web/sf web/
-  && mv lib/vendor/symfony1-1.4.20 lib/vendor/symfony
+unzip lib/vendor/packages/symfony1-1.4.20.zip -d lib/vendor/
+cp -r lib/vendor/symfony1-1.4.20/data/web/sf web/
+mv lib/vendor/symfony1-1.4.20 lib/vendor/symfony
